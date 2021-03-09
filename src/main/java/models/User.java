f253e899ddc740eb9a1505618403f361c027ae78
MUTHAKABRIAN <muthakabrian@gmail.com>
2021-03-09 11:04:07 +0300

    add user model

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -2,35 +2,47 @@ package models;
 
 import java.util.Objects;
 
-public class Users {
+public class User{
     private int id;
-    private String role;
-    private String position;
     private int departmentId;
+    private String position;
+    private String role;
     private String name;
 
-    public Users(String role, String position, int departmentId, String name) {
-        this.role = role;
-        this.position = position;
+    public User( int departmentId, String position, String role, String name){
         this.departmentId = departmentId;
+        this.position = position;
+        this.role = role;
         this.name = name;
-        this.id = id;
     }
+
+
 
     public int getId() {
         return id;
+    }
+
+    public String getName(){
+
+        return name;
+    }
+
+    public void setName(String name){
+
+        this.name = name;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
-    public String getRole() {
-        return role;
+    public int getDepartmentId() {
+        return departmentId;
     }
 
-    public void setRole(String role) {
-        this.role = role;
+    public void setDepartmentId(int departmentId) {
+
+        this.departmentId = departmentId;
     }
 
     public String getPosition() {
@@ -41,50 +53,30 @@ public class Users {
         this.position = position;
     }
 
-    public int getDepartmentId() {
-        return departmentId;
+    public String getRole() {
+        return role;
     }
 
-    public void setDepartmentId(int departmentId) {
-        this.departmentId = departmentId;
+    public void setRole(String role) {
+        this.role = role;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
     @Override
     public boolean equals(Object o) {
         if ( this == o ) return true;
         if ( o == null || getClass () != o.getClass () ) return false;
-        Users users = (Users) o;
-        return id == users.id &&
-                departmentId == users.departmentId &&
-                Objects.equals ( position, users.position ) &&
-                Objects.equals ( role, users.role ) &&
-                Objects.equals ( name, users.name );
+        User user = (User) o;
+        return id == user.id &&
+                Objects.equals (departmentId, user.departmentId) &&
+                Objects.equals ( position, user.position ) &&
+                Objects.equals ( role, user.role ) &&
+                Objects.equals ( name, user.name );
     }
+
     @Override
     public int hashCode() {
-        return Objects.hash(id, departmentId, position, role, name);
-    }
-    public int departmentId() {
-        return 0;
-    }
-        public int getUserName() {
-            return 0;
-        }
-
-        public int getUserPosition() {
-            return 0;
-        }
-
-        public int getUserRole() {
-            return 0;
-        }
+        return Objects.hash ( id, departmentId, position, role, name );
     }
 
 
+}
