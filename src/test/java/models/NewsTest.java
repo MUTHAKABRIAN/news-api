c55f17061aa7aed1e8f6d18b22695e7ae9bce3aa
MUTHAKABRIAN <muthakabrian@gmail.com>
2021-03-09 11:06:36 +0300

    add news test

--- src/test/java/models/NewsTest.java
+++ src/test/java/models/NewsTest.java
@@ -1,17 +1,50 @@
-package models;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class NewsTest {
-
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-}
+//
+//package models;
+//
+//import org.junit.After;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import static org.junit.Assert.*;
+//
+//public class NewsTest {
+//
+//    @Before
+//    public void setUp() throws Exception {
+//    }
+//
+//    @After
+//    public void tearDown() throws Exception {
+//    }
+//
+//    @Test
+//    public void getType() {
+//        News testNews = setupNews();
+////        assertEquals("Great service", testNews.getType ());
+//    }
+//
+//    @Test
+//    public void setContent() {
+//        News testNews = setupNews();
+//        testNews.setContent("No free dessert :(");
+////        assertNotEquals("Great service", testNews.getContent());
+//    }
+//
+////    private News setupNews() {
+////        return null;
+////    }
+//
+//    @Test
+//    public void getPostdate() {
+//        News testNews = setupNews ();
+////        assertEquals("Kim", testNews.getPostdate());
+//    }
+//
+//
+//
+//    // helper
+//    public News setupNews (){
+//        return new News ("Great service", "Kim", 4, 1);
+//    }
+//
+//}
