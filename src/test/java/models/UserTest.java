c4c6d6e2f7f2db50f0539e4edbefaf58a9a26707
MUTHAKABRIAN <muthakabrian@gmail.com>
2021-03-09 11:07:30 +0300

    create user test

--- src/test/java/models/UserTest.java
+++ src/test/java/models/UserTest.java
@@ -1,17 +1,52 @@
-package models;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class UserTest {
-
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-}
+//
+//package models;
+//
+//import org.junit.After;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import static org.junit.Assert.*;
+//
+//public class UserTest {
+//
+//    @Before
+//    public void setUp() throws Exception {
+//    }
+//
+//    @After
+//    public void tearDown() throws Exception {
+//    }
+//
+//    @Test
+//    public void getNameReturnsCorrectName() throws Exception {
+//        User testUser = setupUser();
+////        assertEquals("Fish Witch",testUser.getName());
+//    }
+//
+//    private User setupUser() {
+//        return null;
+//    }
+//
+//    @Test
+//    public void getAPositionReturnsCorrectPosition() throws Exception {
+//        User testUser = setupUser();
+////        assertEquals("214 NE Broadway",testUser.getPosition());
+//    }
+//
+//    @Test
+//    public void getRoleReturnsCorrectRole() throws Exception {
+//        User testUser = setupUser();
+////        assertEquals("97232",testUser.getRole());
+//    }
+//
+//
+//
+//
+//    public News setupNews (){
+//        return new News("Fish Witch", "214 NE Broadway", "97232", "503-402-9874", "http://fishwitch.com", "hellofishy@fishwitch.com");
+//    }
+//
+//    public News setupAltNews (){
+//        return new News("Fish Witch", "214 NE Broadway", "97232", "503-402-9874");
+//    }
+//}
