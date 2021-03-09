ee66eb766f76adaa871ea85a5ec0b6a31da60792
MUTHAKABRIAN <muthakabrian@gmail.com>
2021-03-09 10:49:35 +0300

    add interface for department

--- src/main/java/dao/DepartmentDAO.java
+++ src/main/java/dao/DepartmentDAO.java
@@ -1,4 +1,21 @@
 package dao;
 
-public interface DepartmentDao {
+import models.Department;
+//import models.DepartmentNews;
+import models.User;
+
+import java.util.List;
+
+public interface DepartmentDAO {
+
+
+    //create
+    void add(Department department);
+
+    //    read
+    List<Department> getAllDepartment();
+
+    Department getDepartmentById(int departmentId);
+
+
 }
