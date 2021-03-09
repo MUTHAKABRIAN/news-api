2ff638eb3f9901097e382bbf80f997d0d6998b32
MUTHAKABRIAN <muthakabrian@gmail.com>
2021-03-09 11:05:52 +0300

    add test department

--- src/test/java/models/DepartmentTest.java
+++ src/test/java/models/DepartmentTest.java
@@ -1,50 +1,45 @@
-package models;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class DepartmentTest {
-
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
-    @Test
-    void getId() {
-    }
-
-    @Test
-    void setId() {
-    }
-
-    @Test
-    void getName() {
-    }
-
-    @Test
-    void setName() {
-    }
-
-    @Test
-    void getDescription() {
-    }
-
-    @Test
-    void setDescription() {
-    }
-
-    @Test
-    void testEquals() {
-    }
-
-    @Test
-    void testHashCode() {
-    }
-}
+//
+//
+//package models;
+//
+//import org.junit.After;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import static org.junit.Assert.*;
+//
+//public class DepartmentTest {
+//
+//    @Before
+//    public void setUp() throws Exception {
+//    }
+//
+//    @After
+//    public void tearDown() throws Exception {
+//    }
+//
+////    @Test
+////    public void getName() {
+////        Department testDepartment = setupDepartment();
+////        assertEquals("dessert", testDepartment.getName());
+////    }
+//
+//    @Test
+//    public void setName() {
+//        Department testDepartment = setupDepartment ();
+//        testDepartment.setName("breakfast");
+//        assertNotEquals("dessert", testDepartment.getName());
+//    }
+////
+//    @Test
+//    public void setId() {
+//        Department testDepartment = setupDepartment ();
+//        testDepartment.setId(5);
+//        assertEquals(5, testDepartment.getId());
+//    }
+//
+//    // helper
+//    public Department setupDepartment(){
+//        return new Department();
+//    }
+//}
